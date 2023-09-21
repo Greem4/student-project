8b0e94b2ebf54573cf55d325790ee3d3d90edc39
Greem4 <i@greem4.ru>
2023-09-21 19:33:56 +0300

    Initial commit

---
+++ src/edu/javacourse/studentorder/StudentOrderValidator.java
@@ -0,0 +1,71 @@
+package edu.javacourse.studentorder;
+
+import edu.javacourse.studentorder.domain.*;
+import edu.javacourse.studentorder.mail.MailSender;
+import edu.javacourse.studentorder.validator.ChildrenValidator;
+import edu.javacourse.studentorder.validator.CityRegisterValidator;
+import edu.javacourse.studentorder.validator.StudentValidator;
+import edu.javacourse.studentorder.validator.WeddingValidator;
+
+import java.util.concurrent.CyclicBarrier;
+
+public class StudentOrderValidator {
+    public static void main(String[] args) {
+        StudentOrder so;
+
+        checkAll();
+    }
+
+    static void checkAll() {
+
+
+        while (true) {
+            StudentOrder so = readStudentOrder();
+            if (so == null) {
+                break;
+            }
+            AnswerCityRegister cityAnswer = checkCityRegister(so);
+            if (!cityAnswer.success) {
+//                continue;
+                break;
+            }
+            AnswerWedding wedAnswer = checkWedding(so);
+            AnswerChildren childAnswer = checkChildren(so);
+            AnswerStudent studentAnswer = checkStudent(so);
+
+            sendMail(so);
+
+        }
+    }
+
+
+    static StudentOrder readStudentOrder() {
+        StudentOrder so = new StudentOrder();
+        return so;
+    }
+
+    static AnswerCityRegister checkCityRegister(StudentOrder so) {
+        CityRegisterValidator crv1 = new CityRegisterValidator();
+        crv1.hostName = "Host1";
+        AnswerCityRegister ans1 = crv1.checkCityRegister(so);
+        return ans1;
+    }
+
+    static AnswerWedding checkWedding(StudentOrder so) {
+        WeddingValidator wd = new WeddingValidator();
+        return wd.checkWedding(so);
+    }
+
+    static AnswerChildren checkChildren(StudentOrder so) {
+        ChildrenValidator cv = new ChildrenValidator();
+        return cv.checkChildren(so);
+    }
+
+    static AnswerStudent checkStudent(StudentOrder so) {
+        return new StudentValidator().checkStudent(so);
+    }
+
+    static void sendMail(StudentOrder so) {
+        new MailSender().sendMail(so);
+    }
+}
