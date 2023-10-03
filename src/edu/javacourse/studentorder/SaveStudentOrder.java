bd0611a2842b8f9148156696d76ed145d7d999e9
Greem4 <i@greem4.ru>
2023-10-03 11:21:50 +0300

    R 41 working with the database

--- src/edu/javacourse/studentorder/SaveStudentOrder.java
+++ src/edu/javacourse/studentorder/SaveStudentOrder.java
@@ -1,17 +1,17 @@
 package edu.javacourse.studentorder;
 
-import edu.javacourse.studentorder.dao.DirectoryDao;
+import edu.javacourse.studentorder.dao.DictionaryDaoImpl;
 import edu.javacourse.studentorder.domain.*;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.Statement;
 import java.time.LocalDate;
+import java.util.List;
 
 public class SaveStudentOrder {
     public static void main(String[] args) throws Exception {
-
+        List<Street> d = new DictionaryDaoImpl().findStreets("sec");
+        for (Street s : d) {
+            System.out.println(s.getStreetName());
+        }
 
 //        StudentOrder s = buildStudentOrder(10);
 //        StudentOrder so = new StudentOrder();
