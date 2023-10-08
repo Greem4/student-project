5e602ef4e0b792348f64cd8ecae4da83bd9da139
Greem4 <i@greem4.ru>
2023-10-08 15:52:59 +0300

    R 56, connecting the tables

--- student_project.sql
+++ student_project.sql
@@ -1,53 +1,50 @@
-drop table if exists jc_student_child;
-drop table if exists jc_student_order;
-drop table if exists jc_passport_office;
-drop table if exists jc_register_office;
-drop table if exists jc_country_struct;
-drop table if exists jc_university;
-drop table if exists jc_street;
+DROP TABLE IF EXISTS jc_student_child;
+DROP TABLE IF EXISTS jc_student_order;
+DROP TABLE IF EXISTS jc_passport_office;
+DROP TABLE IF EXISTS jc_register_office;
+DROP TABLE IF EXISTS jc_country_struct;
+DROP TABLE IF EXISTS jc_university;
+DROP TABLE IF EXISTS jc_street;
 
-
-
-create table jc_street
+CREATE TABLE jc_street
 (
     street_code integer not null,
     street_name varchar(300),
-    primary key (street_code)
+    PRIMARY KEY (street_code)
 );
-create table jc_university
+
+CREATE TABLE jc_university
 (
     university_id   integer not null,
     university_name varchar(300),
-    primary key (university_id)
+    PRIMARY KEY (university_id)
 );
 
-
-create table jc_country_struct
+CREATE TABLE jc_country_struct
 (
-
     area_id   char(12) not null,
     area_name varchar(200),
     PRIMARY KEY (area_id)
 );
 
-create table jc_passport_office
+CREATE TABLE jc_passport_office
 (
-
     p_office_id      integer  not null,
     p_office_area_id char(12) not null,
     p_office_name    varchar(200),
     PRIMARY KEY (p_office_id),
-    FOREIGN KEY (p_office_area_id) REFERENCES jc_country_struct (area_id) on delete restrict
+    FOREIGN KEY (p_office_area_id) REFERENCES jc_country_struct (area_id) ON DELETE RESTRICT
 );
 
-create table jc_register_office
+CREATE TABLE jc_register_office
 (
     r_office_id      integer  not null,
     r_office_area_id char(12) not null,
     r_office_name    varchar(200),
     PRIMARY KEY (r_office_id),
-    FOREIGN KEY (r_office_area_id) REFERENCES jc_country_struct (area_id) on delete restrict
+    FOREIGN KEY (r_office_area_id) REFERENCES jc_country_struct (area_id) ON DELETE RESTRICT
 );
+
 
 CREATE TABLE jc_student_order
 (
@@ -90,10 +87,10 @@ CREATE TABLE jc_student_order
     PRIMARY KEY (student_order_id),
     FOREIGN KEY (h_street_code) REFERENCES jc_street (street_code) ON DELETE RESTRICT,
     FOREIGN KEY (h_passport_office_id) REFERENCES jc_passport_office (p_office_id) ON DELETE RESTRICT,
-    FOREIGN KEY (h_university_id) REFERENCES jc_university (university_id)  ON DELETE RESTRICT,
+    FOREIGN KEY (h_university_id) REFERENCES jc_university (university_id) ON DELETE RESTRICT,
     FOREIGN KEY (w_street_code) REFERENCES jc_street (street_code) ON DELETE RESTRICT,
     FOREIGN KEY (w_passport_office_id) REFERENCES jc_passport_office (p_office_id) ON DELETE RESTRICT,
-    FOREIGN KEY (w_university_id) REFERENCES jc_university (university_id)  ON DELETE RESTRICT,
+    FOREIGN KEY (w_university_id) REFERENCES jc_university (university_id) ON DELETE RESTRICT,
     FOREIGN KEY (register_office_id) REFERENCES jc_register_office (r_office_id) ON DELETE RESTRICT
 );
 
@@ -102,7 +99,7 @@ CREATE TABLE jc_student_child
     student_child_id     SERIAL,
     student_order_id     integer      not null,
     c_sur_name           varchar(100) not null,
-    c_given_name          varchar(100) not null,
+    c_given_name         varchar(100) not null,
     c_patronymic         varchar(100) not null,
     c_date_of_birth      date         not null,
     c_certificate_number varchar(10)  not null,
